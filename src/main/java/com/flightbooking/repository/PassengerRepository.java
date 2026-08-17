d39bd37f4ea0e05c41e8005bcaaa16bd54b223d2
suryamoulika52 <suryamoulika23@gmail.com>
2026-08-17 16:28:59 +0530

    Update PassengerRepository.java

--- src/main/java/com/flightbooking/repository/PassengerRepository.java
+++ src/main/java/com/flightbooking/repository/PassengerRepository.java
@@ -1,8 +1,14 @@
+
 package com.flightbooking.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.flightbooking.entity.Passenger;
 
 public interface PassengerRepository extends JpaRepository<Passenger, Long> {
+
+    List<Passenger> findByActiveTrue();
 
 }
