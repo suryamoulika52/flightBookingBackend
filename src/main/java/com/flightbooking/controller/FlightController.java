90a5aebd24586c6bf3ec44b175f6902bdcef2d2c
suryamoulika52 <suryamoulika23@gmail.com>
2026-08-17 14:06:42 +0530

    Add Vercel frontend URL to CORS origins
    Added a new allowed origin for CORS in FlightController.

--- src/main/java/com/flightbooking/controller/FlightController.java
+++ src/main/java/com/flightbooking/controller/FlightController.java
@@ -19,7 +19,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 	    "http://localhost:5173",
 	    "http://localhost:5176",
 	    "http://localhost:5174",
-	    "http://localhost:5175"
+	    "http://localhost:5175",
+	"https://flight-booking-frontend-psi.vercel.app"
 	})
 @RestController
 @RequestMapping("/api/flights")
